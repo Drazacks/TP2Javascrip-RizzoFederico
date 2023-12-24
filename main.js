e834a456aba0f9c27b2218109735bf524c4cc75a
Federico Rizzo <federico_rizzo@hotmail.com>
2023-12-24 00:10:38 -0300

    java2.1

--- main.js
+++ main.js
@@ -1,15 +1,23 @@
-const searchInput = document.getElementById('searchInput');
+// Obtener la referencia al input de búsqueda y al contenido de las cards
+const searchBar = document.querySelector('.search-bar');
 const cards = document.querySelectorAll('.card');
 
-searchInput.addEventListener('input', function () {
-    const searchText = searchInput.value.toLowerCase();
-
-    cards.forEach(card => {
-        const cardText = card.textContent.toLowerCase();
-        if (cardText.includes(searchText)) {
+// Función para filtrar las cards según el texto ingresado en el input
+const filtrarCards = (texto) => {
+    const textoBusqueda = texto.toLowerCase();
+    cards.forEach((card) => {
+        const titulo = card.querySelector('h2').innerText.toLowerCase();
+        const descripcion = card.querySelector('p').innerText.toLowerCase();
+        if (titulo.includes(textoBusqueda) || descripcion.includes(textoBusqueda)) {
             card.style.display = 'block';
         } else {
             card.style.display = 'none';
         }
     });
+};
+
+// Evento al escribir en el input de búsqueda
+searchBar.addEventListener('input', (e) => {
+    const textoIngresado = e.target.value.trim();
+    filtrarCards(textoIngresado);
 });
