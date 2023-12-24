c96cf865c64bc2d3311792b40d72795c62b8a26d
Federico Rizzo <federico_rizzo@hotmail.com>
2023-12-23 23:26:10 -0300

    java2.0

---
+++ main.js
@@ -0,0 +1,15 @@
+const searchInput = document.getElementById('searchInput');
+const cards = document.querySelectorAll('.card');
+
+searchInput.addEventListener('input', function () {
+    const searchText = searchInput.value.toLowerCase();
+
+    cards.forEach(card => {
+        const cardText = card.textContent.toLowerCase();
+        if (cardText.includes(searchText)) {
+            card.style.display = 'block';
+        } else {
+            card.style.display = 'none';
+        }
+    });
+});
